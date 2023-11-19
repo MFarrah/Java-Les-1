806cef87e3a6b7fe8fb343deca4275e738f28a0e
Michael Farrah Girgis <149530935+MFarrah@users.noreply.github.com>
2023-11-19 23:58:52 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
     public class Main {
         public static void main(String[] args) {
@@ -6,33 +6,30 @@ import java.util.Scanner;
             int number; number = 6;
             int input1; input1 = 4;
             int input2; input2 = 20;
-            String name; name = "Micha Farrah Girgis!";
-
-            //insert behaviour//
-
-
-
-
-
+            String name; name = "Micha Farrah Girgis";
 
             hello(name);
             positiveOrNegative(number);
             postiveOrZeroOrNegativ(number);
             bartender(number);
             sum(input1, input2);
+
+            //insert behaviour//
+
+
         }
 
       //methods section//
-  public static void hello(String name) {
-    System.out.println("Hello, " + name);
+  public static void hello(String name){
+    System.out.println("Hello, " + name +"!");
   }
 
-  public static void positiveOrNegative(int number) {
+  public static void positiveOrNegative(int number){
     if ( number>0 ){
     System.out.println("This number is positive!");}
     else {
-    System.out.println("This number is negative!");
-  }}
+    System.out.println("This number is negative!");}
+  }
 
   public static void postiveOrZeroOrNegativ (int number){
     if ( number>0 ){
@@ -45,19 +42,18 @@ import java.util.Scanner;
 
   public static void bartender (int number){
 
-  switch (number){
-  case 1:
-  System.out.println("You selected option 1");
-  break;
-  case 2:
-  System.out.println("You selected option 2");
-  break;
-  case 3:
-  System.out.println("You selected option 3");
-  break;
-  default:
-  System.out.println("Invalid choice");
-  }
+    switch (number){
+    case 1:
+    System.out.println("You selected option 1");
+    break;
+    case 2:
+    System.out.println("You selected option 2");
+    break;
+    case 3:
+    System.out.println("You selected option 3");
+    break;
+    default:
+    System.out.println("Invalid choice");}
   }
 
   public static void sum (int input1, int input2){
@@ -65,6 +61,5 @@ import java.util.Scanner;
     int result = (input1 + input2);
       System.out.println("4 + 20 = " + result);
 
-  }
-
+    }
   }
